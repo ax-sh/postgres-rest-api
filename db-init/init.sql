b7497bff4347b5a7da1dcb52709074a69c74a561
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-18 13:39:47 +0545

    style(db-init/init.sql): adjust table creation syntax

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,6 +1,19 @@
-CREATE DATABASE IF NOT EXISTS public;
-USE public;
-CREATE TABLE IF NOT EXISTS public.user (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL
-);
+-- CREATE DATABASE IF NOT EXISTS public;
+-- USE public;
+-- CREATE TABLE public.user (
+--     id SERIAL PRIMARY KEY,
+--     username VARCHAR(50) UNIQUE NOT NULL
+-- );
+
+CREATE TABLE IF NOT EXISTS public."user"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."user"
+    OWNER to postgres;
+
+
+ALTER TABLE IF EXISTS public."user"
+    ADD COLUMN username character varying COLLATE pg_catalog."default";
