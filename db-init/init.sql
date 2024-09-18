f3bf6e897788361fcdcfba53ce2bc9d2324e073f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-18 14:48:25 +0545

    chore(db-init): clean up init.sql and update user insertion

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,25 +1,4 @@
--- -- CREATE DATABASE IF NOT EXISTS public;
--- -- USE public;
--- -- CREATE TABLE public.user (
--- --     id SERIAL PRIMARY KEY,
--- --     username VARCHAR(50) UNIQUE NOT NULL
--- -- );
---
--- CREATE TABLE IF NOT EXISTS public."user"
--- (
--- )
---
--- TABLESPACE pg_default;
---
--- ALTER TABLE IF EXISTS public."user"
---     OWNER to postgres;
---
---
--- ALTER TABLE IF EXISTS public."user"
---     ADD COLUMN username character varying COLLATE pg_catalog."default";
---
--- INSERT INTO public."user" (username)a
--- VALUES ('Ford');
+-- https://postgrest.org/en/v12/tutorials/tut1.html
 
 CREATE TABLE IF NOT EXISTS public.users
 (
@@ -33,4 +12,5 @@ ALTER TABLE IF EXISTS public.users
     OWNER to postgres;
 
 INSERT INTO public.users (username, fullName)
-VALUES ('jon', 'do');
+VALUES ('jon', 'do') ON CONFLICT (username)
+                     DO NOTHING;
