b9713424a150251efa3b986879df788b41d3629e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-18 14:40:34 +0545

    refactor(sql): update user table structure and data insertion

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,19 +1,36 @@
--- CREATE DATABASE IF NOT EXISTS public;
--- USE public;
--- CREATE TABLE public.user (
---     id SERIAL PRIMARY KEY,
---     username VARCHAR(50) UNIQUE NOT NULL
--- );
+-- -- CREATE DATABASE IF NOT EXISTS public;
+-- -- USE public;
+-- -- CREATE TABLE public.user (
+-- --     id SERIAL PRIMARY KEY,
+-- --     username VARCHAR(50) UNIQUE NOT NULL
+-- -- );
+--
+-- CREATE TABLE IF NOT EXISTS public."user"
+-- (
+-- )
+--
+-- TABLESPACE pg_default;
+--
+-- ALTER TABLE IF EXISTS public."user"
+--     OWNER to postgres;
+--
+--
+-- ALTER TABLE IF EXISTS public."user"
+--     ADD COLUMN username character varying COLLATE pg_catalog."default";
+--
+-- INSERT INTO public."user" (username)a
+-- VALUES ('Ford');
 
-CREATE TABLE IF NOT EXISTS public."user"
+CREATE TABLE IF NOT EXISTS public.users
 (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    fullName VARCHAR(50) NULL
 )
-
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public."user"
+ALTER TABLE IF EXISTS public.users
     OWNER to postgres;
 
-
-ALTER TABLE IF EXISTS public."user"
-    ADD COLUMN username character varying COLLATE pg_catalog."default";
+INSERT INTO public.users (username, fullName)
+VALUES ('jon', 'do');
